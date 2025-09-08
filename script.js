0c610bdfe4428772ba162f677121e8eb20a0ea51
AnoopKumarJaiswal <anoopjaiswal1220@gmail.com>
2025-09-08 07:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-// complete this js code
-
-
-class Person{
-	constructor(n ,a)
-	{
-		this.name = n
-		this.age = a
-	}
-
-
-	greet()
-	{
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`)
-	}
+function Person(name, age) 
+{
+	this.name = name
+	this.age = age
 }
 
-
-class Employee extends Person{
-	constructor( n, a,jt)
-	{
-		super(n ,a)
-		this.jobTitle = jt
-	}
-
-
-	jobGreet()
-	{
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-	}
+Person.pototype.greet = function  () => {
+	console.lo(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
-// function Person(name, age) {}
 
-// function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle)
+	{
+		super(name ,age)
+		this.jobTitle = jobTitle
+	}
+
+Employee.prototype.jobGreet =  function () =>
+	{
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and job title is ${this.jobTitle}`)
+	}
 
 // Do not change code below this line
 window.Person = Person;
